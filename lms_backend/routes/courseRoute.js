30c5cecf2d9b50fe0df5b10ecaa9268f8b8e5130
David Guragain <guragaindavid18@gmail.com>
2025-07-30 15:30:08 +0545

    Delete lms_backend directory
    deleted unwanted backend component

--- lms_backend/routes/courseRoute.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router()
-const courseController = require("../controllers/courseController")
-const auth = require("../middleware/authguard")
-
-router.post("/createCourse", courseController.createCourse)
-router.get("/getallcourse", courseController.getAllCourse)
-
-module.exports = router
